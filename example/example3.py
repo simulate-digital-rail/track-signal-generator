589790b6bd3371d27cb4fcbc119972c8f146d32f
Saturnhafen <37272151+SaturnHafen@users.noreply.github.com>
2023-01-31 22:39:09 +0100

    Create examples with switches

--- example/example3.py
+++ example/example3.py
@@ -9,27 +9,28 @@ from track_signal_generator.generator import TrackSignalGenerator
 
 def setup() -> Topology:
     node1 = Node()
-    switch = Node()
     node2 = Node()
+    switch = Node()
     node3 = Node()
 
     node1.geo_node = DbrefGeoNode(0, 10)
+    node2.geo_node = DbrefGeoNode(0, 20)
     switch.geo_node = DbrefGeoNode(50, 10)
-    node2.geo_node = DbrefGeoNode(100, 10)
-    node3.geo_node = DbrefGeoNode(100, 20)
+    node3.geo_node = DbrefGeoNode(100, 10)
 
     edge1 = Edge(node1, switch, length=50)
-    edge2 = Edge(switch, node2, length=50)
+    edge2 = Edge(node2, switch, length=50)
     edge3 = Edge(switch, node3, length=50)
 
     node1.set_connection_head(switch)
-    switch.set_connection_head(node2)
-    switch.set_connection_left(node3)
+    node2.set_connection_head(switch)
+    switch.set_connection_head(node3)
+    #switch.set_connection_right(node2)
 
     topology = Topology()
     topology.add_node(node1)
-    topology.add_node(switch)
     topology.add_node(node2)
+    topology.add_node(switch)
     topology.add_node(node3)
     topology.add_edge(edge1)
     topology.add_edge(edge2)
